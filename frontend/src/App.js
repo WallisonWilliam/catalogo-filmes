5db9899157c878afa919a795064e7bac7120dd4b
Wallison William <wallisonwilliamm@gmail.com>
2023-05-10 23:24:51 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Container, Row, Col, Card, Carousel } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Container, Row, Col, Card, Carousel } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const MovieCard = ({ movie }) => {
   const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
@@ -11,16 +11,14 @@ const MovieCard = ({ movie }) => {
     <Col xs={6} sm={4} md={3} lg={2}>
       <Card className="mb-4">
         <Card.Img variant="top" src={imageUrl} />
-        <Card.Body>
-          <Card.Title>{movie.title}</Card.Title>
-          <Card.Text>{movie.vote_average} / 10
-          </Card.Text>
-        </Card.Body>
+        {/* <Card.Body>
+        <Card.Title>{movie.title}</Card.Title>
+        <Card.Text>{movie.vote_average} / 10</Card.Text>
+      </Card.Body> */}
       </Card>
     </Col>
   );
 }; // fim do Card de filme
-
 
 const App = () => {
   const [featuredMovie, setFeaturedMovie] = useState(null);
@@ -29,17 +27,21 @@ const App = () => {
 
   useEffect(() => {
     const fetchFeaturedMovie = async () => {
-      const response = await axios.get('http://localhost:3001/featured');
+      const response = await axios.get("http://localhost:3001/featured");
       setFeaturedMovie(response.data);
     };
 
     const fetchTopRatedMovies = async () => {
-      const response = await axios.get('http://localhost:3001/movies/top-rated');
+      const response = await axios.get(
+        "http://localhost:3001/movies/top-rated"
+      );
       setTopRatedMovies(response.data);
     };
 
     const fetchNowPlayingMovies = async () => {
-      const response = await axios.get('http://localhost:3001/movies/now-playing');
+      const response = await axios.get(
+        "http://localhost:3001/movies/now-playing"
+      );
       setNowPlayingMovies(response.data);
     };
 
@@ -50,13 +52,17 @@ const App = () => {
 
   const featuredMovieImageUrl = featuredMovie
     ? `https://image.tmdb.org/t/p/original${featuredMovie.backdrop_path}`
-    : '';
+    : "";
 
   return (
     <Container>
       <Carousel>
         <Carousel.Item>
-          <img className="d-block w-100" src={featuredMovieImageUrl} alt={featuredMovie?.title} />
+          <img
+            className="d-block w-100"
+            src={featuredMovieImageUrl}
+            alt={featuredMovie?.title}
+          />
           <Carousel.Caption>
             <h3>{featuredMovie?.title}</h3>
             <p>{featuredMovie?.overview}</p>
